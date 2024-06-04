e4b86bdca5c663312813819047b314cfff526444
mohammadafandi76 <mohammadafandi76@gmail.com>
2024-06-04 02:59:38 +0000

    fix focus on bottom navigation that can be select on focus

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -8,26 +8,52 @@ import '../features/home/presentation/screens/home_screen.dart';
 import 'route_name.dart';
 
 class AppRouter {
+  String lastRoute = "";
   Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case RouteName.HOME:
-        return MaterialPageRoute(
-            builder: (context) => const HomeScreen(), settings: routeSettings);
+        if (lastRoute != RouteName.HOME) {
+          lastRoute = RouteName.HOME;
+          return MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+              settings: routeSettings);
+        }
+        break;
+
       case RouteName.BUILDING:
-        return MaterialPageRoute(
-            builder: (context) => const BuildingScreen(),
-            settings: routeSettings);
+        if (lastRoute != RouteName.BUILDING) {
+          lastRoute = RouteName.BUILDING;
+          return MaterialPageRoute(
+              builder: (context) => const BuildingScreen(),
+              settings: routeSettings);
+        }
+        break;
+
       case RouteName.DOCUMENTS:
-        return MaterialPageRoute(
-            builder: (context) => const DocumentScreen(),
-            settings: routeSettings);
+        if (lastRoute != RouteName.DOCUMENTS) {
+          lastRoute = RouteName.DOCUMENTS;
+          return MaterialPageRoute(
+              builder: (context) => const DocumentScreen(),
+              settings: routeSettings);
+        }
+        break;
+
       case RouteName.CART:
-        return MaterialPageRoute(
-            builder: (context) => const CartScreen(), settings: routeSettings);
+        if (lastRoute != RouteName.CART) {
+          lastRoute = RouteName.CART;
+          return MaterialPageRoute(
+              builder: (context) => const CartScreen(),
+              settings: routeSettings);
+        }
+        break;
       case RouteName.ACCOUNT:
-        return MaterialPageRoute(
-            builder: (context) => const AccountScreen(),
-            settings: routeSettings);
+        if (lastRoute != RouteName.ACCOUNT) {
+          lastRoute = RouteName.ACCOUNT;
+          return MaterialPageRoute(
+              builder: (context) => const AccountScreen(),
+              settings: routeSettings);
+        }
+        break;
     }
     return null;
   }
